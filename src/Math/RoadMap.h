860b8cb7cc4c704b24da30b4147946155fce2b7e
Karamaz0V1 <florent.guiotte@gmail.com>
2016-02-03 11:09:53 +0100

    Get a position replace get velocity function

--- src/Math/RoadMap.h
+++ src/Math/RoadMap.h
@@ -12,7 +12,7 @@ namespace Math {
 			RoadMap(Math::Vector2<Config::Real> & targetPosition = Math::Vector2<Config::Real>(15.0, 15.0), std::vector<Math::Vector2<Config::Real> > & agentsPositions = std::vector<Math::Vector2<Config::Real> >());
 			virtual ~RoadMap();
 			Math::Vector2<Config::Real> getTargetWay(Math::Vector2<Config::Real> const & worldCoordinates) const;
-			Math::Vector2<Config::Real> getPositionTowardTarget(Math::Vector2<Config::Real> const & worldCoordinates, const double & agentSpeed, const Config::Real & dt) const;
+			Math::Vector2<Config::Real> getPositionTowardTarget(Math::Vector2<Config::Real> & velocity, const Math::Vector2<Config::Real> const & worldCoordinates, const double & agentSpeed, const Config::Real & dt) const;
 
 		private:
 			//std::array<int, 100> _roadMap; // TODO: recreate game map space in array cf. RandomAgent
--- src/Math/src/RoadMap.cpp
+++ src/Math/src/RoadMap.cpp
@@ -102,39 +102,62 @@ namespace Math
 
 	}
 
-	Vector2<Real> RoadMap::getPositionTowardTarget(Vector2<Real> const & worldCoordinates, const double & agentSpeed, const Real & dt) const {
-		double dist = agentSpeed * dt;
+	Math::Vector2<Config::Real> RoadMap::getPositionTowardTarget(Math::Vector2<Config::Real> & velocity, const Math::Vector2<Config::Real> const & worldCoordinates, const double & agentSpeed, const Config::Real & dt) const {
+		double maxdist = agentSpeed * dt;
 		const Map * cmap = OgreFramework::GlobalConfiguration::getCurrentMap();
 		Vector2<int> gridCoordinates = cmap->toGridCoordinates(worldCoordinates);
 		
 
 		// TODO: return target when possible
 		// TODO: while dist > 0 : find next best cell; if dist - cellCost > 0 : select cell then iter;
-		if (_map[gridCoordinates] == 1) {
-			cout << "[RoadMap] Normalement t'es arrivé gros." << endl;
-			return Vector2<Real>();
-		}
 
-		Vector2<Real> bestWay;
-		double bestWayScore = _map[gridCoordinates];
+		double dist = 0;
+		Vector2<int> newGridCoordinates = gridCoordinates;
+		bool pathfind = false;
 
-		// Shit happens
-		if (bestWayScore == 0) bestWayScore = numeric_limits<double>::max();
+		while (pathfind == false) {
 
-		for (int i = -1; i <= 1; i++)
-			for (int j = -1; j <= 1; j++) {
-				if (j == 0 && i == 0) continue;
-				Vector2<int> neighbour(gridCoordinates[0] + i, gridCoordinates[1] + j);
-				if (! _map.isValid(neighbour)) continue;
-				if (_map[neighbour] > bestWayScore) continue;
-				if (_map[neighbour] == bestWayScore && abs(i) == abs(j)) continue;
-				bestWayScore = _map[neighbour];
-				// Todo: set bestWay direction refactor
-				bestWay[0] = i ;//- j * 1.0f / 2;
-				bestWay[1] = j ;//- i * 1.0f / 2;
+			if (_map[newGridCoordinates] == 1) {
+				cout << "[RoadMap] Normalement t'es arrivé gros." << endl;
+				Vector2<int> ivelocity(newGridCoordinates - gridCoordinates);
+				velocity[0] = ivelocity[0];
+				velocity[1] = ivelocity[1];
+				velocity = velocity.normalized();
+				return cmap->toWorldCoordinates(newGridCoordinates);
 			}
 
-		return bestWay.normalized();
+			Vector2<int> bestNeighbour; // \o~~ 
+			double bestWayScore = _map[gridCoordinates];
+
+			//// Shit happens
+			//if (bestWayScore == 0) bestWayScore = numeric_limits<double>::max();
+
+			for (int i = -1; i <= 1; i++)
+				for (int j = -1; j <= 1; j++) {
+					if (j == 0 && i == 0) continue;
+					Vector2<int> neighbour(newGridCoordinates[0] + i, newGridCoordinates[1] + j);
+					if (! _map.isValid(neighbour)) continue;
+					if (_map[neighbour] > bestWayScore) continue;
+					if (_map[neighbour] == bestWayScore && abs(i) == abs(j)) continue;
+					bestWayScore = _map[neighbour];
+					bestNeighbour = neighbour;
+				}
+
+			if (dist + _map[newGridCoordinates] - _map[bestNeighbour] <= maxdist) {
+				newGridCoordinates += bestNeighbour;
+				dist +=  _map[newGridCoordinates] - _map[bestNeighbour];
+			} else {
+				pathfind = true;
+			}
+
+		}
+
+		//cout << "[RoadMap] Normalement t'es arrivé gros." << endl;
+		Vector2<int> ivelocity(newGridCoordinates - gridCoordinates);
+		velocity[0] = ivelocity[0];
+		velocity[1] = ivelocity[1];
+		velocity = velocity.normalized();
+		return cmap->toWorldCoordinates(newGridCoordinates);
 	}
 
 }
